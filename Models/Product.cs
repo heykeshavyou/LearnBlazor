7796ca4fbca8177583515d8228e8f73a59b97bff
Keshav <keshav210305@gmail.com>
2025-02-20 19:39:20 +0530

    1. Data Binding

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<products_prop>products { get; set; }
+
+    }
+}
---
+++ Models/products_prop.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnBlazor.Models
+{
+    public class products_prop
+    {
+        public  int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
